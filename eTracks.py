d545d5d2f5648fdccebd6662685665af695131ac
Audrey <audrey.farrell@stonybrook.edu>
2019-09-26 10:19:11 -0400

    Added comments on units and logic to main tracking script, fixed plots to have accurate unit labels on axes, added user input functionality

--- eTracks.py
+++ eTracks.py
@@ -22,19 +22,18 @@ W = np.sqrt(N*EC**2/(M_E*EP_0))   #plasma frequency in 1/s
 #     -Returns position at t+dt
 # -Function for plotting the position as a function of xi, on a color
 #   axis of E field
-# -Function for creating animated gif from resultant images
 
 def EField(r):
         #returns the electric field at position r (from Wei Lu's paper)
         return -1.0/4.0 * r
 
 def Momentum(r, dt, p_0):
-        #Returns the momentum at t + dt
+        #Returns the momentum at t + dt, in units of m_e c
         return p_0 +  EField(r) * dt
 
 def Velocity(p):
-        #returns the velocity from the momentum
-        return p/M_E
+        #returns the velocity from the momentum, in units of c
+        return p
 
 def GetTrajectory(r_0,p_0):
         #returns array of r v. t
@@ -44,20 +43,25 @@ def GetTrajectory(r_0,p_0):
         rn = r_0 # position in c/w_p
         pn = p_0 # momentum in m_e c
         vn = Velocity(pn) # velocity in c
-        t = 0.0
-        dt = .0001
+        t = 0.0 # start time in 1/w_p
+        dt = .0001 # time step in 1/w_p
 
         old_r = r_0 - 1.0
-
+        
+        #Iterate through position and time using a linear approximation 
+        #until the radial position begins decreasing
         while rn > old_r:
-                print("r = " + str(rn) + " c/w_p, v = ", vn)
+#               print("r = " + str(rn) + " c/w_p, v = ", vn)
 
+                #Determine Momentum and velocity at this time and position
                 pn = Momentum(rn, dt, pn)
-                vn = pn
+                vn = Velocity(pn)
                 
+                #Add former data points to the data lists
                 r_dat.append(rn)
                 t_dat.append(t)
 
+                #Add the distance traveled in dt to r, increase t by dt
                 old_r = rn
                 rn += vn*dt
                 t += dt
@@ -66,14 +70,16 @@ def GetTrajectory(r_0,p_0):
 
 
 def main():
-        #create nxn array of xi v. r, with values of the electric field
-        r_dat, t_dat = GetTrajectory(0.8,0.3)
+        #Get initial position and momentum from user input:
+        r_0 = float(input("Initial radius (c/w_p): "))
+        p_0 = float(input("Initial transverse momentum (m_e c): "))
         
+        #Determine trajectory, creates n-length lists of data points
+        r_dat, t_dat = GetTrajectory(r_0,p_0)
+        #Get number of data points
         n = len(r_dat)
-
-        E_dat = n * [0]
-        
-        for i in range(n):
-                E_dat[i] = n * [EField(r_dat[i])]
+        #Create list of E values for each transverse position
+        E_dat = list([EField(r) for r in r_dat]) 
+        #Plot the trajectory
         plotTracks.plot(r_dat, t_dat, E_dat)
 main()
--- plotTracks.py
+++ plotTracks.py
@@ -5,15 +5,20 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import seaborn as sea
 import matplotlib.cm as cm
-def plot(r,t,E):
-        
-        plt.style.use('seaborn-poster')
-        #ax1 = sea.heatmap(E, cmap = "YlGnBu")
-        
-        #ax2 = ax1.twinx()
 
-        plt.plot(t,r)
-        plt.xlabel("time (1/w_p")
-        plt.ylabel("radius (c/w_p)")
-        plt.title("Electron Radial Trajectory")
+def plot(r,t,E):
+
+        plt.style.use('seaborn-poster')
+        cmap = mpl.cm.get_cmap("YlGnBu")
+        normalize = mpl.colors.Normalize(vmin=min(E),vmax=max(E))
+        colors = [cmap(normalize(value)) for value in E]
+
+        fig, ax = plt.subplots(figsize=(10,10))
+        ax.plot(t,r)
+        #cax, _ = mpl.colorbar.make_axes(ax)
+        #cax.set_title("E Field (m_e c \omega_p / c)")
+        #cbar = mpl.colorbar.ColorbarBase(cax,cmap=cmap, norm=normalize)
+        ax.set_xlabel("time ($\omega_p^{-1}$)")
+        ax.set_ylabel("radius ($c/\omega_p$)")
+        ax.set_title("Electron Radial Trajectory")
         plt.show()
