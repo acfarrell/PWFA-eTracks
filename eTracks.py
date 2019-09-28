c7d1cccc83633990237614c5a49d76a70bdc97aa
Audrey <audrey.farrell@stonybrook.edu>
2019-09-28 12:26:31 -0400

    Added color axis for plotting electric field on radial trajectory plot.

--- eTracks.py
+++ eTracks.py
@@ -15,71 +15,63 @@ C = 299892458                     #speed of light in vacuum in m/s
 N = 1e23                          #electron number density in 1/m^3
 W = np.sqrt(N*EC**2/(M_E*EP_0))   #plasma frequency in 1/s
 
-#Code Structure:
-# -Function for determining transverse E Field
-# -Function for determining radial position at time t
-#     -Takes input of present time position (t, r) and time step dt
-#     -Returns position at t+dt
-# -Function for plotting the position as a function of xi, on a color
-#   axis of E field
-
 def EField(r):
-        #returns the electric field at position r (from Wei Lu's paper)
-        return -1.0/4.0 * r
+  #returns the electric field at position r (from Wei Lu's paper)
+  return -1.0/4.0 * r
 
 def Momentum(r, dt, p_0):
-        #Returns the momentum at t + dt, in units of m_e c
-        return p_0 +  EField(r) * dt
+  #Returns the momentum at t + dt, in units of m_e 
+  return p_0 +  EField(r) * dt
 
 def Velocity(p):
-        #returns the velocity from the momentum, in units of c
-        return p
+  #returns the velocity from the momentum, in units of c
+  return p
 
 def GetTrajectory(r_0,p_0):
-        #returns array of r v. t
-        r_dat = []
-        t_dat = []
+  #returns array of r v. t
+  r_dat = []
+  t_dat = []
 
-        rn = r_0 # position in c/w_p
-        pn = p_0 # momentum in m_e c
-        vn = Velocity(pn) # velocity in c
-        t = 0.0 # start time in 1/w_p
-        dt = .01 # time step in 1/w_p
+  rn = r_0 # position in c/w_p
+  pn = p_0 # momentum in m_e c
+  vn = Velocity(pn) # velocity in c
+  t = 0.0 # start time in 1/w_p
+  dt = .01 # time step in 1/w_p
 
-        old_r = r_0 - 1.0
+  old_r = r_0 - 1.0
         
-        #Iterate through position and time using a linear approximation 
-        #until the radial position begins decreasing
-        while rn > old_r:
+  #Iterate through position and time using a linear approximation 
+  #until the radial position begins decreasing
+  while rn > old_r:
 #               print("r = " + str(rn) + " c/w_p, v = ", vn)
 
-                #Determine Momentum and velocity at this time and position
-                pn = Momentum(rn, dt, pn)
-                vn = Velocity(pn)
-                
-                #Add former data points to the data lists
-                r_dat.append(rn)
-                t_dat.append(t)
+  #Determine Momentum and velocity at this time and position
+    pn = Momentum(rn, dt, pn)
+    vn = Velocity(pn)
+          
+    #Add former data points to the data lists
+    r_dat.append(rn)
+    t_dat.append(t)
 
-                #Add the distance traveled in dt to r, increase t by dt
-                old_r = rn
-                rn += vn*dt
-                t += dt
-        print("\n Turn Radius = ",rn)
-        return r_dat,t_dat        
+    #Add the distance traveled in dt to r, increase t by dt
+    old_r = rn
+    rn += vn*dt
+    t += dt
+  print("\n Turn Radius = ",rn)
+  return r_dat,t_dat        
 
 
 def main():
-        #Get initial position and momentum from user input:
-        r_0 = float(input("Initial radius (c/w_p): "))
-        p_0 = float(input("Initial transverse momentum (m_e c): "))
+  #Get initial position and momentum from user input:
+  r_0 = float(input("Initial radius (c/w_p): "))
+  p_0 = float(input("Initial transverse momentum (m_e c): "))
         
-        #Determine trajectory, creates n-length lists of data points
-        r_dat, t_dat = GetTrajectory(r_0,p_0)
-        #Get number of data points
-        n = len(r_dat)
-        #Create list of E values for each transverse position
-        E_dat = [[EField(r) for r in r_dat] for t in t_dat] 
-        #Plot the trajectory
-        plotTracks.plot(r_dat, t_dat, E_dat)
+  #Determine trajectory, creates n-length lists of data points
+  r_dat, t_dat = GetTrajectory(r_0,p_0)
+  #Get number of data points
+  n = len(r_dat)
+  #Create list of E values for each transverse position
+  E_dat = [[-1.0*EField(r) for t in t_dat] for r in r_dat] 
+  #Plot the trajectory
+  plotTracks.plot(r_dat, t_dat, E_dat)
 main()
--- plotTracks.py
+++ plotTracks.py
@@ -7,18 +7,18 @@ import seaborn as sea
 import matplotlib.cm as cm
 
 def plot(r,t,E):
-        print("plotting")
-        plt.style.use('seaborn-poster')
-        #cmap = mpl.cm.get_cmap("YlGnBu")
-        #normalize = mpl.colors.Normalize(vmin=min(E),vmax=max(E))
-        #colors = [cmap(normalize(value)) for value in E]
-        
-        fig, ax = plt.subplots(figsize=(10,10))
-        
-        ax.plot(t,r)
-        #plt.contourf(t,r,np.transpose(E),origin='lower',cmap="YlGnBu",alpha=0.5)
-        #plt.colorbar()
-        ax.set_xlabel("time ($\omega_p^{-1}$)")
-        ax.set_ylabel("radius ($c/\omega_p$)")
-        ax.set_title("Electron Radial Trajectory")
-        plt.show()
+  plt.style.use('seaborn-poster')
+
+  fig, ax = plt.subplots(figsize=(10,10))
+  #Make color axis of electric field
+  colors = ax.contourf(t,r,E,levels=100,cmap="YlGnBu",alpha=0.5)
+  #Plot radial position v. time
+  ax.plot(t,r,'k')
+  
+  #Labels
+  cbar = fig.colorbar(colors,ax=ax)
+  cbar.set_label('Electric Field ($m_e c\omega_p/e$)')
+  ax.set_xlabel("Time ($\omega_p^{-1}$)")
+  ax.set_ylabel("Radius ($c/\omega_p$)")
+  ax.set_title("Electron Radial Trajectory")
+  plt.show()
