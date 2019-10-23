6cb0fe29a81d15528c801901e20b408c7e01774e
Audrey <audrey.farrell@stonybrook.edu>
2019-10-22 22:06:58 -0400

    Added in simulation of e- trajectories using simulated transverse electric field from OSIRIS, and began implementing plotting of these fields on top of the tracks. Plotting still needs work

--- eTracks.py
+++ eTracks.py
@@ -4,6 +4,7 @@
 #   electron-driven plasma wakefield accelerators.
 
 import sys
+import math
 import numpy as np
 import plotTracks 
 import h5py as h5
@@ -18,51 +19,6 @@ C = 299892458                     #speed of light in vacuum in m/s
 N = 1e23                          #electron number density in 1/m^3
 W = np.sqrt(N*EC**2/(M_E*EP_0))   #plasma frequency in 1/s
 
-def EField(r):
-  #returns the electric field at position r (from Wei Lu's paper)
-  return -1.0/4.0 * r
-
-def Momentum(r, dt, p_0):
-  #Returns the momentum at t + dt, in units of m_e 
-  return p_0 +  EField(r) * dt
-
-def Velocity(p):
-  #returns the velocity from the momentum, in units of c
-  return p
-
-def GetTrajectory(r_0,p_0):
-  #returns array of r v. t
-  r_dat = []
-  t_dat = []
-
-  rn = r_0 # position in c/w_p
-  pn = p_0 # momentum in m_e c
-  vn = Velocity(pn) # velocity in c
-  t = 0.0 # start time in 1/w_p
-  dt = .01 # time step in 1/w_p
-
-  old_r = r_0 - 1.0
-        
-  #Iterate through position and time using a linear approximation 
-  #until the radial position begins decreasing
-  while rn > old_r:
-#               print("r = " + str(rn) + " c/w_p, v = ", vn)
-
-  #Determine Momentum and velocity at this time and position
-    pn = Momentum(rn, dt, pn)
-    vn = Velocity(pn)
-          
-    #Add former data points to the data lists
-    r_dat.append(rn)
-    t_dat.append(t)
-
-    #Add the distance traveled in dt to r, increase t by dt
-    old_r = rn
-    rn += vn*dt
-    t += dt
-  print("\n Turn Radius = ",rn)
-  return r_dat,t_dat        
-
 def getEfromSim():
   f = h5.File("simulated_data/fields/e2-000066.h5","r")
   datasetNames = [n for n in f.keys()] #Two Datasets: AXIS and e2
@@ -71,30 +27,98 @@ def getEfromSim():
   a1_bounds = f['AXIS']['AXIS1']#.astype(float)
   a2_bounds = f['AXIS']['AXIS2']#.astype(float)
   
-  a1_dat = np.linspace(a1_bounds[0],a1_bounds[1],len(E_dat[0]))
-  a2_dat = np.linspace(a2_bounds[0],a2_bounds[1],len(E_dat))
+  z_dat = np.linspace(a1_bounds[0],a1_bounds[1],len(E_dat[0]))
+  r_dat = np.linspace(a2_bounds[0],a2_bounds[1],len(E_dat))
 
-  fig, ax = plt.subplots()
-  colors = ax.pcolormesh(a1_dat,a2_dat,E_dat,norm=col.SymLogNorm(linthresh=0.03,linscale=0.03,vmin=E_dat.min(),vmax=E_dat.max()),cmap="RdBu_r")
-  cbar = fig.colorbar(colors,ax=ax)
   
-  cbar.set_label('Electric Field ($m_e c\omega_p / e$)')
-  ax.set_xlabel('z ($c/\omega_p$)')
-  ax.set_ylabel('r ($c/\omega_p$)')
-  ax.set_title('Transverse Electric Field from Simulation')
-  plt.show()
+#  fig, ax = plt.subplots()
+#  colors = ax.pcolormesh(z_dat,r_dat,E_dat,norm=col.SymLogNorm(linthresh=0.03,linscale=0.03,vmin=E_dat.min(),vmax=E_dat.max()),cmap="RdBu_r")
+#  cbar = fig.colorbar(colors,ax=ax)
+#  
+#  cbar.set_label('Electric Field ($m_e c\omega_p / e$)')
+#  ax.set_xlabel('z ($c/\omega_p$)')
+#  ax.set_ylabel('r ($c/\omega_p$)')
+#  ax.set_title('Transverse Electric Field from Simulation')
+  #plt.show()
+  return r_dat, z_dat, E_dat
+
+r_sim, z_sim, E_sim = getEfromSim()
+
+def EField(r,z,SHMmodel):
+  #SHMmodel = true returns the electric field at position r (from Wei Lu's paper)
+  #SHMmodel = false returns the simulated data from OSIRIS
+  if SHMmodel:
+    return -1.0/4.0 * r
+  else:
+    zDex = find_nearest_index(z_sim, z)
+    rDex = find_nearest_index(r_sim, r)
+    return E_sim[rDex][zDex]
+
+def Momentum(r, z, dt, p_0,model):
+  #Returns the momentum at t + dt, in units of m_e 
+  return p_0 +  EField(r,z,model) * dt
+
+def Velocity(p):
+  #returns the velocity from the momentum, in units of c
+  return p
+
+def GetTrajectory(r_0,p_0,SHM):
+  #returns array of r v. t
+  r_dat = []
+  z_dat = []
+  t_dat = []
+
+  rn = r_0 # position in c/w_p
+  pn = p_0 # momentum in m_e c
+  vn = Velocity(pn) # velocity in c
+  t = 0.0 # start time in 1/w_p
+  dt = .001 # time step in 1/w_p
+  zn = GetInitialZ() 
+  print("Initial z = ",zn)
+  old_r = r_0 - 1.0
+        
+  #Iterate through position and time using a linear approximation 
+  #until the radial position begins decreasing
+  while rn > old_r:
+
+  #Determine Momentum and velocity at this time and position
+    pn = Momentum(rn, zn, dt, pn,SHM)
+    vn = Velocity(pn)
+          
+    #Add former data points to the data lists
+    r_dat.append(rn)
+    t_dat.append(t)
+    z_dat.append(zn)
+    #print("z = ", zn)
+    #Add the distance traveled in dt to r, increase t by dt
+    old_r = rn
+    zn += dt
+    rn += vn*dt
+    t += dt
+  print("\n Turn Radius = ",rn)
+  return r_dat,z_dat,t_dat        
+
+def GetInitialZ():
+  mindex = np.argwhere(E_sim == np.min(E_sim))
+  return z_sim[mindex[0][0]]
+
+def find_nearest_index(array,value):
+    idx = np.searchsorted(array, value, side="left")
+    if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
+        return idx-1
+    else:
+        return idx
 def main():
+  #Print out program description and instructions:
+
   #Get initial position and momentum from user input:
   r_0 = float(input("Initial radius (c/w_p): "))
-  p_0 = float(input("Initial transverse momentum (m_e c): "))
-        
+  p_0 = float(input("Initial transverse momentum (m_e c): "))        
   #Determine trajectory, creates n-length lists of data points
-  r_dat, t_dat = GetTrajectory(r_0,p_0)
+  r_dat, z_dat, t_dat = GetTrajectory(r_0,p_0,False)
   #Get number of data points
-  n = len(r_dat)
+  #n = len(r_dat)
   #Create list of E values for each transverse position
-  E_dat = [[-1.0*EField(r) for t in t_dat] for r in r_dat] 
   #Plot the trajectory
-  plotTracks.plot(r_dat, t_dat, E_dat)
-#main()
-getEfromSim()
+  plotTracks.plot(r_dat,z_dat, t_dat, E_sim, r_sim,z_sim,False)
+main()
--- plotTracks.py
+++ plotTracks.py
@@ -1,24 +1,35 @@
 #Script for generating plots of electron trajectories
 
 import numpy as np
+import matplotlib.colors as col
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import seaborn as sea
 import matplotlib.cm as cm
 
-def plot(r,t,E):
+def plot(r,z,t,E,r_sim,z_sim,SHM):
   plt.style.use('seaborn-poster')
 
   fig, ax = plt.subplots(figsize=(10,10))
   #Make color axis of electric field
-  colors = ax.contourf(t,r,E,levels=100,cmap="YlGnBu",alpha=0.5)
+  if SHM:
+    colors = ax.contourf(t,r,E,levels=100,cmap="YlGnBu",alpha=0.5)
+    ax.plot(t,r,'k')
+  else:
+    #colors = ax.pcolormesh(z_sim,r_sim,np.transpose(E),norm=col.SymLogNorm(linthresh=0.03,linscale=0.03,vmin=E.min(),vmax=E.max()),cmap="RdBu_r")
+    #cbar = fig.colorbar(colors,ax=ax)
+  
+    #cbar.set_label('Electric Field ($m_e c\omega_p / e$)')
+    ax.set_xlabel('z ($c/\omega_p$)')
+    ax.set_ylabel('r ($c/\omega_p$)')
+    ax.set_title('Transverse Electric Field from Simulation')
+    ax.plot(z,r,'k')
   #Plot radial position v. time
-  ax.plot(t,r,'k')
   
   #Labels
-  cbar = fig.colorbar(colors,ax=ax)
-  cbar.set_label('Electric Field ($m_e c\omega_p/e$)')
-  ax.set_xlabel("Time ($\omega_p^{-1}$)")
-  ax.set_ylabel("Radius ($c/\omega_p$)")
+  #cbar = fig.colorbar(colors,ax=ax)
+  #cbar.set_label('Electric Field ($m_e c\omega_p/e$)')
+  #ax.set_xlabel("Time ($\omega_p^{-1}$)")
+  #ax.set_ylabel("Radius ($c/\omega_p$)")
   ax.set_title("Electron Radial Trajectory")
   plt.show()
