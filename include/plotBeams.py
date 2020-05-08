403dd04e54356b653f0ac9c758e146d28082a052
Audrey Claire Farrell <oods@pop-os.localdomain>
2020-05-08 13:52:09 -0400

    Added function for plotting error in beam profiles

--- include/plotBeams.py
+++ include/plotBeams.py
@@ -35,6 +35,7 @@ def plot():
 
   ax.set_ylabel('r ($c/\omega_p$)')
   ax.set_title('Captured Electron Ionization Positions')
+  ax.set_xlabel("$\\xi$ ($c/\omega_p$)")
   
   plt.xlim(xi[0], xi[-1])
   fn = "capturedRegion.png"
@@ -73,9 +74,11 @@ def plotBeams():
 
   axs[0].set_ylabel('r ($c/\omega_p$)')
   axs[0].set_title('Captured Electron Beam Test')
+  axs[0].set_xlabel("$\\xi$ ($c/\omega_p$)")
   
-  nbins = int(len(E[0])/20)
-  axs[1].hist(drive, bins = nbins,color='red',label="Driving Beam")
+  binwidth = (max(drive) - min(drive))/20.0
+  nbins = int( (max(trail) - min(trail))/binwidth)
+  axs[1].hist(drive, bins = 20,color='red',label="Driving Beam")
   axs[1].hist(trail, bins = nbins,color='blue',label="Trailing Beam")
   counts, bin_edges = np.histogram(trail,bins=nbins)
   axs[1].set_xlabel("$\\xi$ ($c/\omega_p$)")
@@ -151,6 +154,79 @@ def plotBeamOverlaps():
   plt.show()
 
   return
-plot()
-plotBeams()
-plotBeamOverlaps()
+
+def getSquareBeam():
+  #Get initial and final electron location mean and error for 6 xi-slices of a square driving beam
+  dat = np.load('data.npz')
+  xi = dat['xi']
+  escaped = dat['esc']
+  trail = dat['beam']
+  drive = []
+  square_drive = [[],[],[],[],[],[],[],[]]
+  square_trail = [[],[],[],[],[],[],[],[]]
+  drive_tot=[]
+  trail_tot=[]
+  counts = [0,0,0,0,0,0,0,0]
+  n = 0
+  for i in range(len(r)):
+    for j in range(len(xi)):
+      if escaped[i,j] == 1:
+        drive.append(xi[j])
+        n += 1
+  while True:
+    if counts[0] >= 200 and counts[5] >= 200 and counts[1] >= 200 and counts[2]>=200 and counts[3]>=200 and counts[4]>=200:
+      break
+    i = random.randint(0,len(drive)-1)
+    start = 6.8
+    end = 7.1
+    if start <= drive[i] <= end:
+      binw = (end - start)/6.0
+      for nbin in range(6):
+        if start + nbin*binw < drive[i] < start + (nbin+1)*binw and counts[nbin] <= 200:
+          square_drive[nbin].append(drive[i])
+          square_trail[nbin].append(trail[i])
+          drive_tot.append(drive[i])
+          trail_tot.append(trail[i])
+          counts[nbin] += 1
+  #Get Stats
+  drive_means = []
+  drive_err = []
+  trail_means = []
+  trail_err = []
+  for nbin in range(6):
+    trail_means.append(np.mean(square_trail[nbin]))
+    drive_means.append(np.mean(square_drive[nbin]))
+    trail_err.append(np.std(square_trail[nbin]))
+    drive_err.append(np.std(square_drive[nbin]))
+  return drive_means, drive_err, trail_means, trail_err
+def plotBeamError():
+  #Plot initial v. final xi position for square driving beam over a number of random trials
+  fig, ax  = plt.subplots()#2,sharex=True)
+
+  #Get Stats
+  drive = []
+  drive_err = []
+  trail = []
+  trail_err = []
+  for nbin in range(100):
+    d,derr,t,terr = getSquareBeam()
+    trail.append(t)
+    drive.append(d)
+    trail_err.append(terr)
+    drive_err.append(derr)
+  #define binary color map
+  ax.errorbar(np.mean(drive,axis=0),np.mean(trail,axis=0),np.mean(trail_err,axis=0),np.mean(drive_err,axis=0), fmt='ro',ecolor='b', ls='none',capsize=5,markersize=5,elinewidth=2, markeredgewidth=2)
+  ax.set_xlabel("$\\xi_i$ ($c/\omega_p$)")
+  ax.set_ylabel("$\\xi_f$ ($c/\omega_p$)")
+  plt.title("Initial v. Final Electron Position")
+  #plt.xlim(xi[0], xi[-1])
+  #plt.ylim(xi[0],xi[-1])
+  fn = "plots/error.png"
+  plt.savefig(fn,dpi=300,transparent=True)
+  plt.show()
+
+  return
+#plot()
+#plotBeams()
+#plotBeamOverlaps()
+plotBeamError()
