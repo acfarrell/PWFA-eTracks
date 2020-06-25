Fixed basic plot of ionization region using ADK formula

include/plotIonizationRegion.py
@@ -9,13 +9,14 @@ import matplotlib.colors as col
 import matplotlib.ticker as ticker
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from tempfile import TemporaryFile as tmp
+from scipy.special import gamma
 
 import getOsirisFields as osiris
 
 #Definition of Constants
 M_E = 9.109e-31                   #electron rest mass in kg
 EC = 1.60217662e-19               #electron charge in C
-EP_0 = 8.854187817                #vacuum permittivity in C/(V m)
+EP_0 = 8.854187817e-12                #vacuum permittivity in C/(V m)
 C = 299892458                     #speed of light in vacuum in m/s
 N = 1e23                          #electron number density in 1/m^3
 WP = np.sqrt(N*EC**2/(M_E*EP_0))   #plasma frequency in 1/s
@@ -32,15 +33,16 @@ def Wdt(E0):
   gsE = 24.5 # unperturbed ground state energy in eV
   Z = 1 # charge number after ionization
   n = 0.746 # effective principal quantum number
-  sigmaz = .94 # beam spread in z
-  dt = 3 * sigmaz * WP / C # maximum duration of ionization
+  sigmaz = .47 # beam spread in z
+  dt = 3*sigmaz / WP # maximum duration of ionization
   
   E = E0 * (M_E * C * WP)/EC * 1e-9  #Convert normalized field to GV/m
 
-  W0 = 1.52e15 * 4**n * gsE / (n * math.gamma(2*n))
+  W0 = 1.52e15 * 4**n * gsE / (n * gamma(2*n)) * (20.5*gsE**(3/2))**(2*n-1)
   
-  W = W0*(20.5*gsE**(3/2)/E)**(2*n-1)*math.exp(-6.83*gsE**(3/2)/E)
-  print(E)
+  W = W0/((E)**(2*n-1)) * math.exp(-6.83*gsE**(3/2)/E)
+  if W*dt > 1:
+    return 1
   return W * dt
 
 def plotIonizationRegion():
@@ -64,8 +66,8 @@ def plotIonizationRegion():
       if E != 0:
         ratio =  Wdt(E)
         #print(ratio)
-        if ratio > 0.1:
-                #        print(ratio)
+        if ratio > .1:
+                #print(ratio)
           eRatio[i][j] = ratio
   
   fig, ax  = plt.subplots(figsize=(9,6))
@@ -74,11 +76,11 @@ def plotIonizationRegion():
   cmap = plt.cm.OrRd
   cmap.set_bad(color = (1,1,1,0))
 
-  colors = ax.pcolormesh(xi ,r,E * M_E * C * WP / EC,norm=col.SymLogNorm(linthresh=0.03,linscale=0.03,vmin=-E.max()*M_E * C*WP/EC,vmax=E.max()*M_E*C*WP/EC),cmap="RdBu_r")
+  colors = ax.pcolormesh(xi ,r,E,norm=col.SymLogNorm(linthresh=0.03,linscale=0.03,vmin=-E.max(),vmax=E.max()),cmap="RdBu_r")
   
   colors2 = ax.pcolormesh(xi ,r,eRatio, cmap=cmap)
-  #tick_locations=[x*0.01 for x in range(2,10)]+ [x*0.01 for x in range(-10,-1)] + [x*0.1 for x in range(-10,10)] +[ x for x in range(-10,10)]
-  cbar = fig.colorbar(colors,ax=ax)#,ticks=tick_locations, format=ticker.LogFormatterMathtext())
+  tick_locations=[x*0.01 for x in range(2,10)]+ [x*0.01 for x in range(-10,-1)] + [x*0.1 for x in range(-10,10)] +[ x for x in range(-10,10)]
+  cbar = fig.colorbar(colors,ax=ax,ticks=tick_locations, format=ticker.LogFormatterMathtext())
   cbar.set_label('$E_r$, Transverse Electric Field ($m_e c\omega_p / e$)')
   cbar2 = fig.colorbar(colors2,ax=ax)
   cbar2.set_label('$W\Delta t$, Fraction of Ionized Atoms')
@@ -87,7 +89,9 @@ def plotIonizationRegion():
   ax.set_title('Ionization Region')
   ax.set_xlabel("$\\xi$ ($c/\omega_p$)")
   
-  plt.xlim(xi[0], xi[-1])
+  #plt.xlim(xi[0], xi[-1])
+  plt.xlim(4,8)
+  plt.ylim(0,4)
   fn = "ionizationRegion.png"
   plt.savefig(fn,dpi=300,transparent=True)
   plt.show()