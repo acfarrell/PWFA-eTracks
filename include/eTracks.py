9098a0fa143ec160bf1c5e464cc6ee538875fbf1
Audrey Claire Farrell <oods@pop-os.localdomain>
2020-07-28 18:08:41 -0400

    Updated ionization region calculation to avoid divide by zero errors

--- include/eTracks.py
+++ include/eTracks.py
@@ -82,6 +82,19 @@ def Momentum(r, xi, dt, pr, pz):
   #print("pz = ",pz,", pr = ",pr)
   return pz, pr, p
 
+def Momentum_NoB(r, xi, dt, pr, pz):
+  p = math.sqrt(pr**2 + pz**2)
+  vr = Velocity(pr,p)
+  vz = Velocity(pz,p)
+
+  Fz = (EField(r, xi, 1)) #+ BForce(r,xi,vz,vr,1))
+  Fr = (EField(r, xi, 2) )#+ BForce(r,xi,vz,vr,2))
+  #print("Fz = ",Fz,", Fr = ",Fr)
+  pz = pz + Fz * dt
+  pr = pr + Fr * dt
+  p = math.sqrt(pr**2 + pz**2)
+  #print("pz = ",pz,", pr = ",pr)
+  return pz, pr, p
 def Velocity(pi,p):
   v = pi / Gamma(p)
   return v
@@ -138,7 +151,76 @@ def GetTrajectory(r_0,xi_0):
     z_dat = np.append(z_dat, zn)
     vz_dat = np.append(vz_dat, vzn)
     xi_dat = np.append(xi_dat, xin)
-    p_dat = np.append(p_dat, p)
+    p_dat = np.append(p_dat, pr)
+    #print("z = ", zn)
+    if rn > turnRad:
+      turnRad = rn
+
+    #print("vz=",vzn) 
+    #Add the distance traveled in dt to r, increase t by dt
+    zn += vzn * dt
+    rn += vrn * dt
+    t += dt
+    xin = zn - t
+    i += 1
+    
+    # Allow for crossing the beam axis
+    if rn < 0:
+      rn = -rn
+      pr = -pr
+    if rn > 6 or xin < 0 or xin > 10:
+      esc = -1
+      break
+    if outOfBounds(rn, xin): 
+      esc = 1
+  xiPos = xin
+  global trajectories
+  data = [r_dat,xi_dat,vz_dat]
+  trajectories.append(data)  #print(esc)
+  del r_dat, xi_dat, z_dat, t_dat
+  return esc, xiPos
+
+def GetTrajectory_NoB(r_0,xi_0):
+  #returns array of r v. t
+  r_dat, z_dat, t_dat, xi_dat, vz_dat,p_dat = np.array([]),np.array([]),np.array([]),np.array([]),np.array([]),np.array([])
+  p = 0
+  rn = r_0 # position in c/w_p
+  pr = 0 # momentum in m_e c
+  vrn = pr/Gamma(p) # velocity in c
+  t = t0 # start time in 1/w_p
+  dt = .005 # time step in 1/w_p
+  
+  z0 = xi_0 + t0
+  zn = xi_0 + t0
+  pz = 0 
+  vzn = pz/Gamma(p) 
+  dvz = 0.0
+  dvr = 0.0
+
+  old_r = r_0 #- 1.0
+  turnRad = r_0
+  xin = xi_0
+  
+  esc = 2
+
+  #Iterate through position and time using a linear approximation 
+  #until the radial position begins decreasing
+  i = 0 #iteration counter
+  # Iterate while electron energy is under 100 MeV
+  while Gamma(p) < 100/.511:
+  
+    #Determine Momentum and velocity at this time and position
+    pz, pr, p = Momentum_NoB(rn, xin, dt, pr, pz)
+    vzn = Velocity(pz,p)  
+    vrn = Velocity(pr,p)
+
+    #Add former data points to the data lists
+    r_dat = np.append(r_dat, rn)
+    t_dat = np.append(t_dat, t)
+    z_dat = np.append(z_dat, zn)
+    vz_dat = np.append(vz_dat, vzn)
+    xi_dat = np.append(xi_dat, xin)
+    p_dat = np.append(p_dat, pr)
     #print("z = ", zn)
     if rn > turnRad:
       turnRad = rn
@@ -208,6 +290,35 @@ def plotVzTest(fname,t0):
   axs[0].set_ylim(0, r_sim[-1])
   axs[0].set_title('Ionized Electron Trajectories, t = '+str(t0)+'$\omega_p^{-1}$')
   fn = "plots/"+fname+"_vz.png"
+  plt.savefig(fn,dpi=300)
+  #plt.show()
+def plotNoBTest(fname,t0):
+      
+  fig, axs = plt.subplots()
+
+  #Make color axis of electric field
+  #colors = axs.pcolormesh(xi_sim,r_sim,Er_sim,norm=col.SymLogNorm(linthresh=0.03,linscale=0.03,vmin=-Er_sim.max(),vmax=Er_sim.max()),cmap="RdBu_r")
+  tick_locations=[x*0.01 for x in range(2,10)]+ [x*0.01 for x in range(-10,-1)] + [x*0.1 for x in range(-10,10)] +[ x for x in range(-10,10)]
+  #cbar = fig.colorbar(colors,ax=axs[0],ticks=tick_locations, format=ticker.LogFormatterMathtext())
+  #cbar.set_label('$E_r$, Transverse Electric Field ($m_e c\omega_p / e$)')    
+  axs.set_xlabel("$\\xi$ ($c/\omega_p$)")
+  axs.set_ylabel('$p_r$ ($m_e c$)')
+  
+  Btrack = trajectories[0]
+  noBtrack = trajectories[1]
+  Br = Btrack[0][:]
+  Bxi = Btrack[1][:]
+  Bpr = Btrack[2][:]
+  noBr = noBtrack[0][:]
+  noBxi = noBtrack[1][:]
+  noBpr = noBtrack[2][:]
+  axs.plot(Bxi,Bpr,'k',label="With B")
+  axs.plot(noBxi,noBpr,'c--',label="Without B")
+  axs.legend()
+  #axs.set_xlim(xi_sim[0], xi_sim[-1])
+  #axs.set_ylim(0, r_sim[-1])
+  axs.set_title('Ionized Electron Trajectories, t = '+str(t0)+'$\omega_p^{-1}$')
+  fn = "plots/"+fname+".png"
   plt.savefig(fn,dpi=300)
   #plt.show()
 def plotTest(fname,t0):
--- include/plotIonizationRegion.py
+++ include/plotIonizationRegion.py
@@ -23,14 +23,16 @@ C = 299892458                     #speed of light in vacuum in m/s
 N = 5e22                          #electron number density in 1/m^3
 WP = np.sqrt(N*EC**2/(M_E*EP_0))   #plasma frequency in 1/s
 
-Er = quickPIC.transE("fields/exslicexz_00000100.h5")
-Ez = quickPIC.longE("fields/ezslicexz_00000100.h5")
+Er = quickPIC.transE("quickPIC/exslicexz_00000100.h5")
+Ez = quickPIC.longE("quickPIC/ezslicexz_00000100.h5")
 
-r,xi = quickPIC.axes("fields/ezslicexz_00000100.h5")
+r,xi = quickPIC.axes("quickPIC/ezslicexz_00000100.h5")
 
 #print((M_E * C * WP)/EC * 1e-9)  #Convert normalized field to GV/m
 
 def W(E0):
+  if E0 == 0.0:
+    return 0
   # Takes an electric field in normalized units and returns the ionization rate in s^-1
   # Define constants for calculating ionization rate for He
   gsE = 24.5 # unperturbed ground state energy in eV
@@ -41,16 +43,19 @@ def W(E0):
 
   W0 = 1.52e15 * 4**n * gsE / (n * gamma(2*n)) * (20.5*gsE**(3/2))**(2*n-1)
   
-  W = W0/((E)**(2*n-1)) * math.exp(-6.83*gsE**(3/2)/E)
+  W = W0/((E)**(2*n-1)) * math.exp(-6.83*(gsE**(3/2))/E)
+  #print(W)
   return W 
 
-def ionRatio(i,n):
+def ionRatio(i,j):
         #dt = 14.7/2
   integral = 0
-  #for n in range(1,j):
-  En = math.sqrt((Er[i,n])**2 + (Ez[i,n])**2)
-  #integral = integral +  W(En) * (xi[n] - xi[n-1])/WP
-  ratio = W(En) * 14.7 /WP #(xi[n] - xi[n-1])/WP #1 - math.exp(-1*integral)
+  for n in range(len(xi)-1,j,-1):
+    En = math.sqrt((Er[i,n])**2 + (Ez[i,n])**2)
+    integral = integral +  W(En) * (xi[n] - xi[n-1])/WP
+  ratio = 1 - math.exp(-1*integral)#
+  #ratio = W(En) * 14.7 /WP  #1 - math.exp(-1*integral)
+  #print(ratio)
   if ratio > 1.0:
     return 1.0
   return ratio
@@ -65,7 +70,7 @@ def plotIonizationRegion():
   eRatio  = np.zeros((len(r),len(xi)))
   #n = 0
   for j in range(len(xi)):
-    for i in range(int(len(r)/2.0), len(r)):
+    for i in range(int(len(r)/2.0), int(3*len(r)/4)):
             #if escaped[i,j] == 1:
 #        ri = r[i]
 #        xii = xi[j]
@@ -77,7 +82,7 @@ def plotIonizationRegion():
       #if Er[i,j] < -0.5:
       ratio =  ionRatio(i,j)
       if ratio > .1:
-                #      print(ratio)
+        #print(ratio)
         eRatio[i][j] = ratio
   
   fig, ax  = plt.subplots(figsize=(9,6))
@@ -107,3 +112,56 @@ def plotIonizationRegion():
   plt.show()
   return
 plotIonizationRegion()
+def plotIonizationAtRadius():
+        #dat = np.load('data.npz')
+  #xi = dat['xi']
+  #escaped = dat['esc']
+  #trail = []#dat['beam']
+  #drive = []
+  eRatio  = np.zeros(len(r))
+  #n = 0
+  for j in range(len(xi)):
+    if abs(xi[j] - 4) <0.01:
+      print("Found xi=4")
+      for i in range(int(len(r)/2.0), len(r)):
+            #if escaped[i,j] == 1:
+#        ri = r[i]
+#        xii = xi[j]
+#        xif = dat['beam'][j]
+#        drive.append(xi[j])
+#        trail.append(dat['beam'][j])
+#        n += 1
+        print('Row ',i, "/",len(r),", Column ", j,"/",len(xi), end="\r", flush=True)
+      #if Er[i,j] < -0.5:
+        ratio =  ionRatio(i,j)
+        #if ratio > .1:
+        #              print(ratio)
+        eRatio[i] = ratio
+  
+  fig, ax  = plt.subplots(figsize=(9,6))
+  E = Er 
+  #eRatio = np.ma.masked_where(eRatio == 0, eRatio)
+  #cmap = plt.cm.OrRd
+  #cmap.set_bad(color = (1,1,1,0))
+
+  #colors = ax.pcolormesh(xi ,r,E,norm=col.SymLogNorm(linthresh=0.03,linscale=0.03,vmin=-E.max(),vmax=E.max()),cmap="RdBu_r")
+  
+  #colors2 = ax.pcolormesh(xi ,r,eRatio, cmap=cmap)
+  tick_locations=[x*0.01 for x in range(2,10)]+ [x*0.01 for x in range(-10,-1)] + [x*0.1 for x in range(-10,10)] +[ x for x in range(-10,10)]
+  #cbar = fig.colorbar(colors,ax=ax,ticks=tick_locations, format=ticker.LogFormatterMathtext())
+  #cbar.set_label('$E_r$, Transverse Electric Field ($m_e c\omega_p / e$)')
+  #cbar2 = fig.colorbar(colors2,ax=ax)
+  #cbar2.set_label('$N_e/N_0$, Fraction of Ionized Atoms')
+  ax.plot(r, eRatio)
+  ax.set_xlabel('r ($c/\omega_p$)')
+  ax.set_title('Ionization Fraction at $\\xi=4$')
+  ax.set_ylabel("Ionization Fraction")
+  
+  #plt.xlim(xi[0], xi[-1])
+  #plt.xlim(5,9)
+  plt.xlim(0,2)#r[-1])
+  fn = "ionizationRatiosvRadius.png"
+  plt.savefig(fn,dpi=300,transparent=True)
+  plt.show()
+  return
+plotIonizationAtRadius()
